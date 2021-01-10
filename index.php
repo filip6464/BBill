8bc349a2bc319780b1cc81295fcb3e62d0b30220
Filip6464 <filip-zyga@wp.pl>
2021-01-10 22:37:10 +0100

    first routing

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "Hello there";
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+//Tablica routingu
+Routing::get('index','DefaultController');
+Routing::get('homepage','DefaultController');
+
+Routing::run($path);
