6a774d13b9850e87b824d6744095e293de8a34cb
Filip6464 <filip-zyga@wp.pl>
2021-01-25 15:08:15 +0100

    getBills fix

---
+++ public/sections/bottomNavBar.php
@@ -0,0 +1,15 @@
+<?php
+echo "
+    <div class=\"navbar\">
+        <a href=\"#\">
+            <i class=\"fas fa-user-friends\"></i>
+        </a>
+        <a href=\"#\">
+            <i class=\"fas fa-history\"></i>
+        </a>
+        <a href=\"#\">
+            <i class=\"fas fa-list\"></i>
+        </a>
+      </div>
+      ";
+?>
