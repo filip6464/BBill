0244255b96b53da4b7d28ea98988214677975b7b
filip6464 <filip-zyga02@wp.pl>
2021-01-30 18:29:02 +0100

    fix css

--- config.php
+++ config.php
@@ -1,7 +1,3 @@
-<?php
 
-const USERNAME = 'eemqnltyfuleys';
-const PASSWORD = '4c31ac7457959cf07ab15ae8f078824240cb562c725c10e9b0d0cf92e75bb369';
-const HOST = 'ec2-46-137-156-205.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'd70go5gd78od0u';
+
 
